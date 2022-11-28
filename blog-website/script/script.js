fa45d3bedfe9aad30a3ec42f8c1e781fddcf58f5
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-11-28 20:47:54 +0530

    added header-navigation, banner, about section and half way on latest post

--- blog-website/script/script.js
+++ blog-website/script/script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+console.log("Hello World");
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.navigation');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+})
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
